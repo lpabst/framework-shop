898ab095429413bcb68a408ddbea470a78012e18
loren pabst <loren pabst>
2017-06-19 14:20:54 -0600

    done with all the steps

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Landing from './components/Landing/Landing.js';
 import Shop from './components/Shop/Shop.js';
 import Details from './components/Details/Details.js';
 import Cart from './components/Cart/Cart.js';
+import ThankYou from './components/ThankYou/ThankYou.js';
 
 export default (
     <Switch>
@@ -12,5 +13,6 @@ export default (
         <Route component={Shop} path='/shop' />
         <Route component={Details} path='/details/:name' />
         <Route component={Cart} path='/cart' />
+        <Route component={ThankYou} path='/thank-you' />
     </Switch>
 );
